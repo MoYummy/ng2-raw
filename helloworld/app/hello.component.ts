a50f284a7b9496dcb91e645106c38156c8b9a96c
Mike Pan <mo_yummy@sina.cn>
2016-12-29 00:46:17 +0800

    Added html

--- helloworld/app/hello.component.ts
+++ helloworld/app/hello.component.ts
@@ -2,10 +2,7 @@ import {Component} from 'angular2/core';
 
 @Component({
     selector: 'hello',
-    template: `
-        <h1>Hello, {{name}}!</h1>
-        Say hello to: <input [value]="name" (input)="name = $event.target.value">
-    `,
+    templateUrl: './app/hello.component.html',
     styleUrls: ['./app/hello.component.css']
 })
 export class HelloComponent {
