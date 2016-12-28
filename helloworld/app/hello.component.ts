34767fd0d3d8afd05a4197cbf98e73c0bf5dd7c0
Mike Pan <mo_yummy@sina.cn>
2016-12-29 00:42:42 +0800

    Updated path of css

--- helloworld/app/hello.component.ts
+++ helloworld/app/hello.component.ts
@@ -5,7 +5,8 @@ import {Component} from 'angular2/core';
     template: `
         <h1>Hello, {{name}}!</h1>
         Say hello to: <input [value]="name" (input)="name = $event.target.value">
-    `
+    `,
+    styleUrls: ['./app/hello.component.css']
 })
 export class HelloComponent {
     name: string = 'ng2';
