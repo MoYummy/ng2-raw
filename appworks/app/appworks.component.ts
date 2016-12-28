882ca1f0eb7a61ae1004422c1010ede1c5c127cf
Mike Pan <mo_yummy@sina.cn>
2016-12-28 20:19:38 +0800

    Added appworks

---
+++ appworks/app/appworks.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './appworks.component.html',
+  styleUrls: ['./appworks.component.css']
+})
+export class AppworksComponent {
+  title = 'app works!';
+}
